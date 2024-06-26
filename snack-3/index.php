14b407e338af00460babf577a7bc050fdd22762a
giordanofabrizii <giordanofabrizi@gmail.com>
2024-06-26 13:27:05 +0200

    added snack 3

---
+++ snack-3/index.php
@@ -0,0 +1,73 @@
+<?php 
+    // Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. 
+    // Stampare ogni data con i relativi post.
+
+    $posts = [
+        '10/01/2019' => [
+            [
+                'title' => 'Post 1',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 1'
+            ],
+            [
+                'title' => 'Post 2',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 2'
+            ],
+        ],
+        '10/02/2019' => [
+            [
+                'title' => 'Post 3',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 3'
+            ]
+        ],
+        '15/05/2019' => [
+            [
+                'title' => 'Post 4',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 4'
+            ],
+            [
+                'title' => 'Post 5',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 5'
+            ],
+            [
+                'title' => 'Post 6',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 6'
+            ]
+        ],
+    ];
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 3</title>
+</head>
+<body>
+    <ul>
+        <?php foreach($posts as $key => $data) { ?>
+        <li>
+            Data: <?php echo $key ?>
+            <ul>
+                <?php foreach($data as $singlePost) { ?>
+
+                <li>
+                    <p> Title: <?php echo $singlePost['title']; ?> </p>
+                    <p> Author: <?php echo $singlePost['author']; ?> </p>
+                    <p> Text: <?php echo $singlePost['text']; ?> </p>
+                </li>
+
+                <?php } ?>
+            </ul>
+        </li>
+        <?php } ?>
+            
+    </ul>
+</body>
+</html>
