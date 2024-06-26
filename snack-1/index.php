a4c5a106a55f066e5be0a66f82dc6e40cc9258f9
giordanofabrizii <giordanofabrizi@gmail.com>
2024-06-26 12:19:17 +0200

    array di partite creato

---
+++ snack-1/index.php
@@ -0,0 +1,34 @@
+<!-- Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario. 
+Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
+Stampiamo a schermo tutte le partite con questo schema:
+Olimpia Milano - Cantù | 55-60 -->
+
+<?php 
+    $partite = [
+        [
+            "squadra1" = "Warriors",
+            "squadra2" = "Lakers",
+            "punteggio" = $rand(1-100) .'-' . $rand(1-100),
+        ],
+        [
+            "squadra1" = "Warriors",
+            "squadra2" = "Lakers",
+            "punteggio" = $rand(1-100) .'-' . $rand(1-100),
+        ],
+        [
+            "squadra1" = "Warriors",
+            "squadra2" = "Lakers",
+            "punteggio" = $rand(1-100) .'-' . $rand(1-100),
+        ],
+        [
+            "squadra1" = "Warriors",
+            "squadra2" = "Lakers",
+            "punteggio" = $rand(1-100) .'-' . $rand(1-100),
+        ],
+        [
+            "squadra1" = "Warriors",
+            "squadra2" = "Lakers",
+            "punteggio" = $rand(1-100) .'-' . $rand(1-100),
+        ],
+    ];
+?>
