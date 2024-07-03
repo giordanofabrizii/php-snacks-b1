17d6093bd032631183d59af2c2385352e2ccaf60
giordanofabrizii <giordanofabrizi@gmail.com>
2024-07-03 12:43:35 +0200

    fixed

--- snack-9/classes/PremiumUser.php
+++ snack-9/classes/PremiumUser.php
@@ -1,10 +1,15 @@
 <?php 
 
+require_once __DIR__ . '/Membership.php';
+
 class premiumUser extends utente{
+    private $membership;
     private $rewards;
 
-    public function __construct($rewards) {
+    public function __construct($nome, $cognome, $username, $email, $rewards, Membership $membership) {
+        parent::__construct($nome, $cognome, $username, $email);
         $this->rewards = $rewards;
+        $this->membership = $membership;
     }
 
     /**
