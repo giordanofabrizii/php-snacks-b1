fee887c9afc58910b14826f8f6ab22742c10c1df
giordanofabrizii <giordanofabrizi@gmail.com>
2024-07-03 12:05:43 +0200

    scaffolding

---
+++ snack-9/index.php
@@ -0,0 +1,13 @@
+<?php  ?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 9</title>
+</head>
+<body>
+    
+</body>
+</html>
