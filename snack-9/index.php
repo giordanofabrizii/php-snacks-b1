aae7c8b6a32803ad21ff9e5970fd045f6c5520fd
giordanofabrizii <giordanofabrizi@gmail.com>
2024-07-03 12:49:07 +0200

    added creation of users

--- snack-9/index.php
+++ snack-9/index.php
@@ -1,4 +1,15 @@
-<?php  ?>
+<?php  
+
+require_once __DIR__ . '/classes/Utente.php';
+require_once __DIR__ . '/classes/PremiumUser.php';
+require_once __DIR__ . '/classes/Membership.php';
+
+$user = new Utente('gino','ginetto','userGina','gino@gina.com');
+$premiumUser = new premiumUser('gino','ginetto','userGina','gino@gina.com','3 coin',new Membership ('84B1893B', '4 months'));
+
+var_dump($user, $premiumUser);
+
+?>
 
 <!DOCTYPE html>
 <html lang="it">
